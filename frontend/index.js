8bae086133ed65a6aa770c28c839f9368663f6c2
Flaviu8 <flavius_flaviu93@yahoo.com>
2022-09-30 21:25:24 +0100

    added header filer index css new restaurant page

--- frontend/index.js
+++ frontend/index.js
@@ -1,28 +1,87 @@
-function filterPosts(e) {
-    const term = e.target.value.toUpperCase();
-    const posts = document.querySelectorAll('.post');
-  
-    posts.forEach(post => {
-      const title = post.querySelector('.post-title').innerText.toUpperCase();
-      const body = post.querySelector('.post-body').innerText.toUpperCase();
-  
-      if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
-        post.style.display = 'flex';
-      } else {
-        post.style.display = 'none';
-      }
-    });
+let tableContainer = document.querySelector(".table-container");
+
+ async function fetchingData () {
+
+  let url = 'http://localhost:3001/restaurants'
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
   }
+
+}
+console.log(fetchingData())
+
+
+
+async function renderListItems()  {
+
+  let data = await fetchingData()
+
+   let html = ''
+
+    data.forEach((table) => {
+    
+      let htmlText = `<div class="table-container">
+               <table class="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">Id</th>
+                              <th scope="col">Restaurants</th>
+                              <th scope="col">City</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td scope="row">${table.id}</td>
+                              <td>${table.name}</td>
+                              <td>${table.city}</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>`;
+       html += htmlText;
+
+       tableContainer.innerHTML = html
+      
+    });
   
-  // Show initial posts
-  showPosts();
+  };
+
+     renderListItems()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function tableSearch() {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.querySelector(".table");
+  tr = table.getElementsByTagName("tr");
+
   
-  window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
-      showLoading();
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
     }
-  });
-  
-  filter.addEventListener('input', filterPosts);
+  }
+}
