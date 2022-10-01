184b9a22b6497c7e1cc64c4a3a13d8101959a0cf
khatuna120 <shaveshovak@gmail.com>
2022-10-01 14:40:04 +0200

    added google map

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ async function renderListItems()  {
                             <div class="modal-body">
                               <img src="${table.picture}" alt="" width="100%" />
                               <p class="mt-4"><b>City</b>: ${table.city_name}<p>
+                              <p> Geo Location </p>
+                              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2591.2552679862915!2d8.466776315797308!3d49.49857326337036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797cc2e0b2a5c4d%3A0x5c6d2270ebe36f9!2sMittelstra%C3%9Fe%2040%2C%2068169%20Mannheim!5e0!3m2!1sen!2sde!4v1664627868887!5m2!1sen!2sde" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
